fff3b4d997135a52dc4387b50b93bd5fdeb81140
@mariaammekarishvili <@mariaammekarishvili>
2023-05-13 18:08:09 +0400

    recunstruct sidebar menu

--- src/configs/navigation.config/index.js
+++ src/configs/navigation.config/index.js
@@ -1,15 +1,7 @@
 import appsNavigationConfig from './apps.navigation.config'
-import uiComponentNavigationConfig from './ui-components.navigation.config'
-import pagesNavigationConfig from './pages.navigation.config'
-import authNavigationConfig from './auth.navigation.config'
-import docNavigationConfig from './doc.navigation.config'
 
 const navigationConfig = [
-    ...appsNavigationConfig,
-    ...uiComponentNavigationConfig,
-    ...pagesNavigationConfig,
-    ...authNavigationConfig,
-    ...docNavigationConfig,
+    ...appsNavigationConfig
 ]
 
 export default navigationConfig
